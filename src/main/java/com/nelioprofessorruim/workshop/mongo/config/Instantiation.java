6ffd8ad66f42c560950bc5f9f127869472fe44b6
Bruno <brunobuenopovoa01@gmail.com>
2024-01-26 15:15:11 -0300

    Operacao de instanciacao da base de dados

---
+++ src/main/java/com/nelioprofessorruim/workshop/mongo/config/Instantiation.java
@@ -0,0 +1,31 @@
+package com.nelioprofessorruim.workshop.mongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.nelioprofessorruim.workshop.mongo.Repository.UserRepository;
+import com.nelioprofessorruim.workshop.mongo.domain.User;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.save(Arrays.asList(maria, alex, bob));
+		
+	}
+
+}
