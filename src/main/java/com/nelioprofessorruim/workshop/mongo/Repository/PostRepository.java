f636eb5d8d64165146b22daf05b412573901ded6
Bruno <brunobuenopovoa01@gmail.com>
2024-01-28 20:19:51 -0300

    Consulta simples com @Query

--- src/main/java/com/nelioprofessorruim/workshop/mongo/Repository/PostRepository.java
+++ src/main/java/com/nelioprofessorruim/workshop/mongo/Repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.nelioprofessorruim.workshop.mongo.Repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.nelioprofessorruim.workshop.mongo.domain.Post;
@@ -10,6 +11,8 @@ import com.nelioprofessorruim.workshop.mongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 	
+	@Query("( 'title: { $regex: ?0, $option: 'i' } }")
+	List<Post> searchTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
